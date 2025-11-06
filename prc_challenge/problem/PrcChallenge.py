8406a5377d2585e8687de62dd3fecbe1fd7c8e08
npo <nico.posocco@hotmail.fr>
2025-11-06 21:30:23 +0100

    on prie pour que les conflits n'existent pas

--- prc_challenge/problem/PrcChallenge.py
+++ prc_challenge/problem/PrcChallenge.py
@@ -5,6 +5,9 @@ from ..utils.load_object_in_file import load_object_in_file
 from ..utils.split_dataset import split_train_val
 from .SupervisedRegression import SupervisedRegression
 from sklearn.metrics import root_mean_squared_error
+import datetime
+import os
+import json
 
 
 class PrcChallenge(SupervisedRegression):
@@ -13,11 +16,13 @@ class PrcChallenge(SupervisedRegression):
         self,
         supervised_fuel_dataset: SupervisedTabularDataset,
         enrichment_dataset: TabularDataset,
+        seed: int,
     ):
 
         train_flights, valid_flights, train_fuel, valid_fuel = split_train_val(
-            0.8, enrichment_dataset, supervised_fuel_dataset
+            0.8, enrichment_dataset, supervised_fuel_dataset, seed=seed,
         )
+        self.seed = seed
         self.train_fuel_X = train_fuel.drop(columns=["fuel_kg"])
         self.train_fuel_Y = train_fuel["fuel_kg"]
         self.test_fuel_X = valid_fuel.drop(columns=["fuel_kg"])
@@ -54,20 +59,36 @@ class PrcChallenge(SupervisedRegression):
     
     def solve_using(self, config):
 
+        run_seed = self.seed
+        run_timestamp = datetime.now().strftime()
+        model = f"{config['model'][0]}({config['model'][1]})"
+        run_name = f"{run_seed}_{run_timestamp}_{model}"
+
+        loaded_config = self.load_config(config)
+
         train_fuel_X, train_enrichment = self.train_fuel_X, self.train_enrichment
 
-        for cleaning_step in config["cleaning"]:
+        for cleaning_step in loaded_config["cleaning"]:
             train_fuel_X, train_enrichment = cleaning_step(
-                train_fuel_X, train_enrichment
+                train_fuel_X, train_enrichment,
             )
 
-        for feature_engineering_step in config["feature_engineering"]:
+        for feature_engineering_step in loaded_config["feature_engineering"]:
             train_fuel_X, train_enrichment = feature_engineering_step(
-                train_fuel_X, train_enrichment
+                train_fuel_X, train_enrichment,
             )
 
         model = config["model"].fit(train_fuel_X, train_enrichment)
-        return model
+
+        evaluation = self.evaluate(model)
+
+        os.makedirs("../../results", exist_ok=True)
+        with open("../../results/config.json", 'w') as fp:
+            json.dump(config, fp)
+        with open("../../results/evaluation.json", 'w') as fp:
+            json.dump(evaluation, fp)
+
+        return model, evaluation
 
     def evaluate(
         self,
