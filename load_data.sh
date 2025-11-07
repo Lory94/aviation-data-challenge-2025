b1ce00a411fd9e61593b90befa0e8026bb98d33c
npo <nico.posocco@hotmail.fr>
2025-11-07 08:36:22 +0100

    corrected bash script for data loading

--- load_data.sh
+++ load_data.sh
@@ -3,5 +3,5 @@ curl --progress-bar -L https://dl.min.io/aistor/mc/release/linux-amd64/mc --crea
 chmod +x ~/aistor-binaries/mc
 
 # Copy data from the bucket
-~/aistor-binaries/mc alias set dc24 https://s3.opensky-network.org/ ACCESS_KEY SECRET_KEY
+~/aistor-binaries/mc alias set dc24 https://s3.opensky-network.org/ $ACCESS_KEY $SECRET_KEY
 ~/aistor-binaries/mc cp --recursive dc24/prc-2025-datasets/  ~/prc-challenge-2025/data
