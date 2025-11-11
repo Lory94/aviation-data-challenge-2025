2261cd029934350e035e32e6820d4996a47ee30e
npo <nico.posocco@hotmail.fr>
2025-11-11 08:33:19 +0100

    removed fit from feature engineering, irrelevant

--- prc_challenge/feature_engineering/BaseFeatureEngineering.py
+++ prc_challenge/feature_engineering/BaseFeatureEngineering.py
@@ -3,8 +3,5 @@ class BaseFeatureEngineering(object):
     def __init__(self, **kwargs):
         self.kwargs = kwargs
 
-    def fit(self, FuelSegment_X, FuelSegment_Y, FlightList, Airport, Flight):
-        pass
-
     def __call__(self, FuelSegment_X, FuelSegment_Y, FlightList, Airport, Flight, column_functions):
         raise NotImplementedError
