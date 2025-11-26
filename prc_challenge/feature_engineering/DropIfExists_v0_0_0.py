e2ed72767561675c128005904b9c7a93434c9272
Thomas Jungers <thomas.jungers@euranova.eu>
2025-11-26 20:52:58 +0100

    Fix DropIfExists

--- prc_challenge/feature_engineering/DropIfExists_v0_0_0.py
+++ prc_challenge/feature_engineering/DropIfExists_v0_0_0.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+
 from .BaseFeatureEngineering import BaseFeatureEngineering
 
 
@@ -7,19 +8,29 @@ class DropIfExists_v0_0_0(BaseFeatureEngineering):
     def __init__(self, columns):
         self.drop_columns = columns
 
-    def __call__(self, FuelSegment_X, FuelSegment_Y, FlightList, Airport, Flight, column_functions):
+    def __call__(
+        self,
+        FuelSegment_X,
+        FuelSegment_Y,
+        FlightList,
+        Airport,
+        Flight,
+        column_functions,
+    ):
 
         non_present_columns = [
-            column for column in self.drop_columns
+            column
+            for column in self.drop_columns
             if column not in FuelSegment_X.columns
         ]
         if len(non_present_columns) > 0:
-            print(f"[DropIfExists] Attempted to drop columns {non_present_columns}, but they are not present.")
-        
-        FuelSegment_X = FuelSegment_X.drop(columns=[
-            col for col in FuelSegment_X 
-            if col in self.drop_columns
-        ])
+            print(
+                f"[DropIfExists] Attempted to drop columns {non_present_columns}, but they are not present."
+            )
+
+        FuelSegment_X = FuelSegment_X.drop(
+            columns=[col for col in FuelSegment_X if col in self.drop_columns]
+        )
 
         # Correcting column_functions
         new_column_functions = defaultdict(list)
@@ -28,4 +39,4 @@ class DropIfExists_v0_0_0(BaseFeatureEngineering):
                 if column not in self.drop_columns:
                     new_column_functions[feature_type].append(column)
 
-        return FuelSegment_X, column_functions
+        return FuelSegment_X, new_column_functions
