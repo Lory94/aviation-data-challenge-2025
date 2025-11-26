d6b459fd8df8cc06c877fbc62449b47a99e48066
lucashenneaux-star <lucas.henneaux@euranova.eu>
2025-11-26 19:32:41 +0100

    fixing

--- prc_challenge/feature_engineering/AddAirportElevation_v0_0_0.py
+++ prc_challenge/feature_engineering/AddAirportElevation_v0_0_0.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from airports import airport_data
 
 from .BaseFeatureEngineering import BaseFeatureEngineering
@@ -14,7 +15,6 @@ class AddAirportElevation_v0_0_0(BaseFeatureEngineering):
         Flight,
         column_functions,
     ):
-
         FuelSegment_X = FuelSegment_X.merge(
             FlightList[["flight_id", "origin_icao", "destination_icao"]],
             on="flight_id",
@@ -23,17 +23,20 @@ class AddAirportElevation_v0_0_0(BaseFeatureEngineering):
 
         origins = set(FuelSegment_X["origin_icao"].unique())
         dests = set(FuelSegment_X["destination_icao"].unique())
-        all_icaos = origins.union(dests)
+        all_icaos = {x for x in origins.union(dests) if pd.notna(x)}
 
         elevation_map = {}
 
         for icao in all_icaos:
+            try:
+                data = airport_data.get_airport_by_icao(icao)
+                if data:
+                    elevation_map[icao] = data[0]["elevation_ft"]
+                else:
+                    elevation_map[icao] = None
+            except Exception:
+                elevation_map[icao] = None
 
-            data = airport_data.get_airport_by_icao(icao)
-
-            elevation_map[icao] = data[0]["elevation_ft"]
-
-        # Application du mapping
         FuelSegment_X["elevation_origin_ft"] = FuelSegment_X["origin_icao"].map(
             elevation_map
         )
@@ -41,9 +44,13 @@ class AddAirportElevation_v0_0_0(BaseFeatureEngineering):
             "destination_icao"
         ].map(elevation_map)
 
+        columns_to_drop = ["origin_icao", "destination_icao"]
+        FuelSegment_X.drop(columns=columns_to_drop, inplace=True)
+
         new_features = [
             "elevation_origin_ft",
             "elevation_destination_ft",
         ]
         column_functions["numerical"].extend(new_features)
+
         return FuelSegment_X, column_functions
