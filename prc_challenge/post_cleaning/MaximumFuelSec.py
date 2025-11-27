f6dcc72010649eb2b2d365f3dc5418f73279e79b
Thomas Jungers <thomas.jungers@euranova.eu>
2025-11-27 07:51:36 +0100

    Fix MaximumFuelSec

--- prc_challenge/post_cleaning/MaximumFuelSec.py
+++ prc_challenge/post_cleaning/MaximumFuelSec.py
@@ -14,15 +14,28 @@ class MaximumFuelSec_v0_0_0(BasePostCleaning):
         self.method = None
         self.save = save
 
-    def __call__(
-        self,
-        FuelSegment_train,
-        FlightList_train,
-        Predictions
-    ):
-        enhanced_flight = pd.merge(FuelSegment_train, FlightList_train,on="flight_id")
-        enhanced_flight["segment_duration"] = (pd.to_datetime(enhanced_flight["end"])-pd.to_datetime(enhanced_flight["start"])).dt.seconds
-        enhanced_flight["fuel_kg_sec"] = enhanced_flight["fuel_kg"]/enhanced_flight["segment_duration"]
-        max_fuel_cons = enhanced_flight.groupby("aircraft_type")["fuel_kg_sec"].max().to_dict()
-        # Aircrafts should consume more than 10% more than the max consumption in the testset and at least 0
-        return Predictions.apply(lambda x: max(0,min(x.y_pred, 1.1*(max_fuel_cons[x.aircraft_type]*x["segment_duration[seconds]"]))),axis=1)
+    def __call__(self, FuelSegment_train, FlightList_train, Predictions):
+        enhanced_flight = pd.merge(FuelSegment_train, FlightList_train, on="flight_id")
+        enhanced_flight["segment_duration"] = (
+            pd.to_datetime(enhanced_flight["end"])
+            - pd.to_datetime(enhanced_flight["start"])
+        ) / pd.Timedelta(seconds=1)
+        enhanced_flight["fuel_kg_sec"] = (
+            enhanced_flight["fuel_kg"] / enhanced_flight["segment_duration"]
+        )
+        max_fuel_cons = (
+            enhanced_flight.groupby("aircraft_type")["fuel_kg_sec"].max().to_dict()
+        )
+        Predictions["segment_duration"] = enhanced_flight["segment_duration"]
+        # Aircrafts should consume no more than 10% more than the max consumption in the
+        # testset and at least 0
+        return Predictions.apply(
+            lambda x: max(
+                0,
+                min(
+                    x.y_pred,
+                    1.1 * (max_fuel_cons[x.aircraft_type] * x["segment_duration"]),
+                ),
+            ),
+            axis=1,
+        )
